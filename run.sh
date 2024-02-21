5773cc9a740087d703f42b8fd18762ec8366e76e
Alonecoder2011 <111146406+Alonecoder2011@users.noreply.github.com>
2024-02-21 13:13:01 -0800

    add runer

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "[!] loading pwa hub...."
+echo "[!] Close window to close the installer (you cant open the installer on your chromebook tho) install it first then close it"
+http-server -o .
